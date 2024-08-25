34221981e6114a3f6f6649bb39c5140f7cf35e1a
QuangHung2712 <112383887+QuangHung2712@users.noreply.github.com>
2024-08-26 00:11:30 +0700

    26/08

---
+++ src/components/LoginView.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-sheet class="bg-deep-purple pa-12" rounded>
+      <h1 class="text-center">Đăng nhập</h1>
+      <v-card class="mx-auto px-6 py-8" max-width="344">
+        <v-form
+          v-model="form"
+          @submit.prevent="onSubmit"
+        >
+          <v-text-field
+            v-model="email"
+            :readonly="loading"
+            :rules="[required]"
+            class="mb-2"
+            label="Email"
+            clearable
+          ></v-text-field>
+  
+          <v-text-field
+            v-model="password"
+            :readonly="loading"
+            :rules="[required]"
+            label="Password"
+            placeholder="Enter your password"
+            :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+            :type="visible ? 'text' : 'password'"
+            @click:append-inner="visible = !visible"
+            clearable
+          ></v-text-field>
+  
+          <br>
+  
+          <v-btn
+            :disabled="!form"
+            :loading="loading"
+            color="success"
+            size="large"
+            type="submit"
+            variant="elevated"
+            block
+          >
+            Sign In
+          </v-btn>
+        </v-form>
+      </v-card>
+    </v-sheet>
+  </template>
+  
+  <script>
+    export default {
+      data: () => ({
+        form: false,
+        email: "",
+        password: "",
+        loading: false,
+        visible: false,
+      }),
+  
+      methods: {
+        onSubmit () {
+          if (!this.form) return
+  
+          this.loading = true
+  
+          setTimeout(() => (this.loading = false), 2000)
+        },
+        required (v) {
+          return !!v || 'Field is required'
+        },
+      },
+    }
+  </script>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,80 @@
 <template>
-  <h1>Đây là Page Home</h1>
+  <v-sheet class="bg-deep-purple pa-12" rounded>
+    <v-card class="mx-auto px-6 py-8" max-width="344">
+      <h1 class="text-center mb-8">Đăng nhập</h1>
+      <v-form
+        v-model="form"
+        @submit.prevent="onSubmit"
+      >
+        <v-text-field
+          v-model="email"
+          :readonly="loading"
+          :rules="[required]"
+          class="mb-2"
+          label="Email"
+          placeholder="Nhập email"
+          clearable
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          :readonly="loading"
+          :rules="[required]"
+          label="Mật khẩu"
+          placeholder="Nhập mật khẩu"
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          @click:append-inner="visible = !visible"
+          clearable
+        ></v-text-field>
+
+        <a
+          class="text-caption text-decoration-none text-blue"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Quên mật khẩu</a>
+
+        <br>
+
+        <v-btn
+          :disabled="!form"
+          :loading="loading"
+          color="success"
+          size="large"
+          type="submit"
+          variant="elevated"
+          block
+        >
+          Đăng nhập
+        </v-btn>
+      </v-form>
+    </v-card>
+  </v-sheet>
 </template>
 
 <script>
+  export default {
+    data: () => ({
+      form: false,
+      email: "",
+      password: "",
+      loading: false,
+      visible: false,
+    }),
 
+    methods: {
+      onSubmit () {
+        if (!this.form) return
 
-export default ({
-  name: 'HomeView',
+        this.loading = true
 
-  components: {
-  },
-});
+        setTimeout(() => (this.loading = false), 2000)
+      },
+      required (v) {
+        return !!v || 'Vui lòng không để trống'
+      },
+    },
+  }
 </script>
